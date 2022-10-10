93939e822d7e77fe4dc470591ded3008f63a6340
athuli7 <mail@athulraj.com>
2022-10-10 21:35:29 +0530

    fix

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-Write-Host "Congratulations! Your first script executed successfully"
+Write-Host "Installing Renzo LPRP"
 Write-Host "Downloading ..."
 if([IntPtr]::Size -eq 8) {
 	$source = "https://github.com/ScaleXY-Clients/lprp-public/releases/latest/download/lprp_windows_x64.zip"
@@ -10,3 +10,4 @@ $destination = '.\install.zip'
 Invoke-RestMethod -Uri $source -OutFile $destination
 unzip .\install.zip
 .\install.bat
+Write-Host "Please restart machine"
